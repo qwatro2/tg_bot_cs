2f0f7d944a70e1ae267e74a0420a53273209e7b7
Si6aClo <sevaaa00@gmail.com>
2022-11-29 23:49:41 +0300

    add currency

--- TgBot/Utils/CurrencyParseUtils.cs
+++ TgBot/Utils/CurrencyParseUtils.cs
@@ -1,11 +1,29 @@
-﻿namespace TgBot.Utils;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using TgBot.DTO;
+
+namespace TgBot.Utils;
 
 public static class CurrencyParseUtils
 {
     private static string _currencyToken;
-    
+
     static CurrencyParseUtils()
     {
-        _currencyToken = Environment.GetEnvironmentVariable("currencyToken") ?? throw new Exception("Token missing");
+        _currencyToken = Environment.GetEnvironmentVariable("currencyToken") ??
+                         throw new Exception("CurrencyToken missing");
+    }
+
+    public static async Task<ResponseCurrencyData?> GetAllCurrency()
+    {
+        var client = new RestClient("https://api.apilayer.com/exchangerates_data/latest?symbols=USD,EUR,CNY&base=RUB");
+        
+        var request = new RestRequest();
+        request.AddHeader("apikey", "M2k6HchxTYEU5rJwi83DBewQhOh0GKUT");
+        RestResponse response = await client.ExecuteAsync(request);
+        
+        var responseData = JsonConvert.DeserializeObject<ResponseCurrencyData>(response.Content);
+        return responseData;
     }
 }
--- TgBot/Utils/MathOperationsUtils.cs
+++ TgBot/Utils/MathOperationsUtils.cs
@@ -4,17 +4,25 @@ public static class MathOperationsUtils
 {
     public static int DoOperations(string[] commandData)
     {
-        var firstNumber = int.Parse(commandData[1]);
-        var secondNumber = int.Parse(commandData[3]);
-        var operation = commandData[2];
-
-        return operation switch
+        try
         {
-            "+" => firstNumber + secondNumber,
-            "-" => firstNumber - secondNumber,
-            "*" => firstNumber * secondNumber,
-            "/" => firstNumber / secondNumber,
-            _ => 0
-        };
+            var firstNumber = int.Parse(commandData[1]);
+            var secondNumber = int.Parse(commandData[3]);
+            var operation = commandData[2];
+
+            return operation switch
+            {
+                "+" => firstNumber + secondNumber,
+                "-" => firstNumber - secondNumber,
+                "*" => firstNumber * secondNumber,
+                "/" => firstNumber / secondNumber,
+                _ => 0
+            };
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return 0;
+        }
     } 
 }
