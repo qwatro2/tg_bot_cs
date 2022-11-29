1bf1f35179da8531d16870f3b821e9941bdb4678
qwatro2 <qwatro2>
2022-11-29 22:20:06 +0300

    Parse words from message

--- TgBot/Utils/MorphAnalyzer.cs
+++ TgBot/Utils/MorphAnalyzer.cs
@@ -1,6 +1,16 @@
-﻿namespace TgBot.Utils;
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TgBot.Utils;
 
 public static class MorphAnalyzer
 {
-    
+    public static Task<Message> DoThisShit(ITelegramBotClient botClient, Message message,
+        CancellationToken cts)
+    {
+        var words = message.Text.Split()
+            .Select(x => string.Concat(x.Select(c => char.IsLetter(c) ? c.ToString() : "")));
+        
+        return botClient.SendTextMessageAsync(message.Chat.Id, string.Join('\n', words), cancellationToken: cts);
+    }
 }
