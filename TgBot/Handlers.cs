0831b543b3fb15665e3dcac4802dcf122bf39f2c
qwatro2 <qwatro2>
2022-11-29 20:56:01 +0300

    initial commit

---
+++ TgBot/Handlers.cs
@@ -0,0 +1,53 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace TgBot;
+
+public class Handlers
+{
+    
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        if (update.Message is not { Text: { } messageText } message)
+            return;
+
+        var chatId = message.Chat.Id;
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+        await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "You said:\n" + messageText,
+            cancellationToken: cancellationToken);
+    }
+    
+    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        // тут можно допилить что-нибудь например запись в логи а не в консоль
+        
+        string errorMessage;
+        
+        if (exception is ApiRequestException apiRequestException)
+        {
+            errorMessage = $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n" +
+                           $"{apiRequestException.Message}";
+        }
+        else
+        {
+            errorMessage = exception.ToString();
+        }
+
+        Console.WriteLine(errorMessage);
+        return Task.CompletedTask;
+    }
+
+    private void MorphAnalysis(Message message)
+    {
+        // тут будет анализ сообщения и типо ручки нужные дергаться будут
+        throw new NotImplementedException();
+    }
+    
+}
---
+++ TgBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TgBot;
+
+class Program
+{
+    public static async Task Main()
+    {
+        var token = Environment.GetEnvironmentVariable("token") ?? throw new Exception("Token missing");
+        var client = new TelegramBotClient(token);
+        var handlers = new Handlers();
+
+        using var cts = new CancellationTokenSource();
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = Array.Empty<UpdateType>()
+        };
+            
+        client.StartReceiving(updateHandler: handlers.HandleUpdateAsync,
+            pollingErrorHandler: handlers.HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token);
+
+        var me = await client.GetMeAsync(cancellationToken: cts.Token);
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+
+        cts.Cancel();
+    }
+}
